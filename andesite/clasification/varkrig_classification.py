67a90b90278fb68d7742b1b7fa9ef7110a71c7e9
Axel <troncoso.axel.g@hotmail.com>
2024-05-22 19:43:14 -0400

    Kriging Variance modification for ANDES

--- andesite/clasification/varkrig_classification.py
+++ andesite/clasification/varkrig_classification.py
@@ -10,7 +10,7 @@ class KrigingVarianceClasification():
         self.variance_col = variance_col
 
 
-    def clasify(self, thresh_cat_1: np.float32, thresh_cat_2) -> GridDatafile:
+    def clasify(self, thresh_cat_1: np.float32, thresh_cat_2, just_results: bool = False) -> GridDatafile:
         self.blockmodel_dataframe = self.blockmodel.load()
         cat_array = da.where((
             self.blockmodel_dataframe[self.variance_col] <= thresh_cat_1) & (self.blockmodel_dataframe[self.estimate_col] >= 0),
@@ -23,5 +23,7 @@ class KrigingVarianceClasification():
                     3,
                 4)
         ))
+        if just_results:
+            return cat_array
         self.blockmodel.add_variable('cat', cat_array)
         return self.blockmodel
