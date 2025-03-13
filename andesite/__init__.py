3cf1f37982c4db33597e6bc0dbe621f167fea5e6
orianam-andesite <oriana.miranda@andesite.cl>
2025-03-13 12:19:04 -0300

    deleted duplicated folder

--- andesite/__init__.py
+++
@@ -1,9 +0,0 @@
-#   -------------------------------------------------------------
-#   Copyright (c) Microsoft Corporation. All rights reserved.
-#   Licensed under the MIT License. See LICENSE in project root for information.
-#   -------------------------------------------------------------
-"""Python Package Template"""
-from __future__ import annotations
-from andesite.utils.version import get_static_version
-
-__version__ = get_static_version()
